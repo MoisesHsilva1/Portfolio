21a68eb051944ce3d5ae8e6048878637e1f98cba
moises henrique <moihgm67@gmail.com>
2025-01-13 00:53:44 -0300

    remove: header

--- src/pages/Portifolio.tsx
+++ src/pages/Portifolio.tsx
@@ -1,16 +1,13 @@
-import Header from "../components/layout/Header";
 import Section from "../components/layout/Section";
 import AboutMe from "../components/aboutMe/AboutMe";
 import Projects from "../components/projects/Projects";
 import TechStack from "../components/techStack/TechStack";
 
 function Portfolio() {
-  const textsHeader = ["Tecnologias", "Projetos", "Contato"];
 
   return (
     <>
       <main className="bg-[#111]">
-        <Header texts={textsHeader} />
         <Section />
         <AboutMe />
         <Projects />
